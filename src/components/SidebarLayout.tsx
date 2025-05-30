0e75d2b4f02c94baff79b4f2dc450a83fb8c6bf6
Sennar2 <danieleraicaldo@gmail.com>
2025-05-30 01:13:22 +0100

    Update SidebarLayout.tsx

--- src/components/SidebarLayout.tsx
+++ src/components/SidebarLayout.tsx
@@ -1,17 +1,17 @@
 import React, { useState } from 'react'
+import { Outlet } from 'react-router-dom'
 import Sidebar from './Sidebar'
 import AppHeader from './AppHeader'
-import { Outlet } from 'react-router-dom'
 
 export default function SidebarLayout() {
   const [isCollapsed, setIsCollapsed] = useState(false)
 
   return (
-    <div className="flex h-screen overflow-hidden">
+    <div className="flex h-screen">
       <Sidebar isCollapsed={isCollapsed} toggleCollapse={() => setIsCollapsed(!isCollapsed)} />
-      <div className="flex flex-col flex-1">
+      <div className="flex-1 flex flex-col ml-16 md:ml-0 transition-all">
         <AppHeader toggleSidebar={() => setIsCollapsed(!isCollapsed)} />
-        <main className="flex-1 overflow-y-auto p-4 bg-gray-100">
+        <main className="flex-1 p-4 bg-gray-100 overflow-auto">
           <Outlet />
         </main>
       </div>
