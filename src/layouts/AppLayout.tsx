0f86fb8f569c48f355278ad126a18a3895b46f9d
Sennar2 <danieleraicaldo@gmail.com>
2025-07-02 14:21:38 +0100

    Update AppLayout.tsx

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
-import Sidebar from '../components/Sidebar'
-import Header from '../components/Header'
+import Sidebar from '../layouts/Sidebar'
+import Header from '../layouts/Header'
 import { Outlet } from 'react-router-dom'
 
 export default function AppLayout() {
