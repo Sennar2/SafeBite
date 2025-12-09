ccbd95bf6995c99bbca31bccb725349da5feb2df
Sennar2 <danieleraicaldo@gmail.com>
2025-12-09 15:14:59 +0000

    Update adminClient.ts

--- src/supabase/adminClient.ts
+++ src/supabase/adminClient.ts
@@ -1,9 +1,11 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Your service role key (go to Supabase > Project Settings > API)
-const supabaseUrl = 'https://ebykgbdqavaqyhxeqrxk.supabase.co'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 const serviceRoleKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVieWtnYmRxYXZhcXloeGVxcnhrIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NzYwNjgyNiwiZXhwIjoyMDYzMTgyODI2fQ.oCOdQSZtILrpErQrgyYfJ_ZNOJJr4XlGhuBbHTZaFrk' // ⚠️ Secure this key — never expose on client
 
 export const adminClient = createClient(supabaseUrl, serviceRoleKey)
+
 
 
