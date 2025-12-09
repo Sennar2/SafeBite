f5925901e39be6142926a8d3f975ba97e6bd6e35
Sennar2 <danieleraicaldo@gmail.com>
2025-12-09 15:14:08 +0000

    Update adminClient.ts

--- src/supabase/adminClient.ts
+++ src/supabase/adminClient.ts
@@ -2,7 +2,8 @@ import { createClient } from '@supabase/supabase-js'
 
 // Your service role key (go to Supabase > Project Settings > API)
 const supabaseUrl = 'https://ebykgbdqavaqyhxeqrxk.supabase.co'
-const serviceRoleKey = 'YOUR_SERVICE_ROLE_KEY' // ⚠️ Secure this key — never expose on client
+const serviceRoleKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVieWtnYmRxYXZhcXloeGVxcnhrIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NzYwNjgyNiwiZXhwIjoyMDYzMTgyODI2fQ.oCOdQSZtILrpErQrgyYfJ_ZNOJJr4XlGhuBbHTZaFrk' // ⚠️ Secure this key — never expose on client
 
 export const adminClient = createClient(supabaseUrl, serviceRoleKey)
+
 
